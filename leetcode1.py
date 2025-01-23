e831857c89afd5512fff061a95ee1f7501cdc691
SOURAV SUMAN <131416432+Luciferxy@users.noreply.github.com>
2025-01-24 05:20:40 +0530

    Add files via upload

---
+++ leetcode1.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+
+        for i in range(len(nums)):
+            d[nums[i]] = i
+
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in d and i != d[x]: 
+                return [i, d[x]]
+
+        return None
